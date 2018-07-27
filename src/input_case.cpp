bcb026c38c27bc910357a60d78f11923c06a3097
Albert Nadal <anadalg@gmail.com>
2018-07-27 02:06:38 +0200

    Updated build script with automatic SDK path detection in MacOS

--- src/input_case.cpp
+++ src/input_case.cpp
@@ -5,24 +5,24 @@
 
 namespace NeuralNetwork {
 
-  class InputCase {
+class InputCase {
 
-  public:
+public:
 
-  TensorFloat *data;
-  TensorFloat *output;
+TensorFloat *data;
+TensorFloat *output;
 
-  InputCase(size_tensor data_size, size_tensor out_size) {
-          data = new TensorFloat(data_size.width, data_size.height, data_size.depth);
-          output = new TensorFloat(out_size.width, out_size.height, out_size.depth);
-  }
+InputCase(size_tensor data_size, size_tensor out_size) {
+        data = new TensorFloat(data_size.width, data_size.height, data_size.depth);
+        output = new TensorFloat(out_size.width, out_size.height, out_size.depth);
+}
 
-  ~InputCase() {
-          delete data;
-          delete output;
-  }
+~InputCase() {
+        delete data;
+        delete output;
+}
 
-  };
+};
 
 }
 
